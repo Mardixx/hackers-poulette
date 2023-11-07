ada8550932e3c9d83cb8988e67e804c903748823
Mardixx <kimi.lefort.pro@gmail.com>
2023-11-07 05:29:24 +0100

    Style and JS

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,13 @@
 /* import { test } from './parts/_validate.js'; */
 
 /* test(); */
-let fname = document.querySelector('.name');
+const fname = document.querySelector('.name');
 fname.addEventListener('keyup', () => {
     let fnameValue = document.querySelector('.name').value;
     console.log(fnameValue);
     if (fnameValue.length < 2) {
-        document.body.style.backgroundColor = 'red';
+        fname.style.border = '5px ridge red';
     } else {
-        document.body.style.backgroundColor = 'green';            
+        fname.style.border = '5px ridge green';            
     }
 })
